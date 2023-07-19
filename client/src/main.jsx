7dd65e3efe9e8c5ac48a98fa08692c5f57fbdf0d
Jakub Iwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-07-20 00:42:58 +0200

    boilerplate

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,53 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+} from "react-router-dom";
+
+import Home from './pages/Home/Home.jsx';
+import Product from './pages/Product/Product.jsx';
+import Products from './pages/Products/Products.jsx'; 
+
+import Navbar from './components/Navbar/Navbar.jsx';
+import Footer from './components/Footer/Footer.jsx';
+
+const Layout = () => {
+  return (
+    <div className='app'>
+      <Navbar/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>,
+      },
+      {
+        path: "/products/:id",
+        element: <Products/>,
+      },
+      {
+        path: "/product/:id",
+        element: <Product/>,
+      },
+    ]
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
