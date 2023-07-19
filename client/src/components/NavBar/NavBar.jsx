afdc7a65cbf0b99d295bfc15f60208c20b48435c
Jakub Iwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-07-20 00:43:13 +0200

    boilerplate

---
+++ client/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <div className='navbar'>
+        <div className="wrapper">
+            <div className='left'>
+
+            </div>
+            <div className='center'>
+
+            </div>
+            <div className='right'>
+                
+            </div>
+        </div>
+    </div>
+  )
+}
