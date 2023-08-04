5c1c48b9e940298d6cbb22b455ed567f9ffca41c
Jakub Iwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2023-08-05 01:16:55 +0200

    a/ categories page with ready filterbar part

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -9,23 +9,29 @@ export default function Products() {
             <input type="checkbox" id="1" value={1} />
             <label htmlFor="1">Miód</label>
           </div>
-        </div>
-        <div className="filterItem">
-          <h2>Product Categories</h2>
           <div className="inputItem">
-            <input type="checkbox" id="1" value={1} />
-            <label htmlFor="1">Miód</label>
+            <input type="checkbox" id="2" value={2} />
+            <label htmlFor="2">Inne</label>
           </div>
         </div>
         <div className="filterItem">
-          <h2>Product Categories</h2>
+          <h2>Sort by</h2>
           <div className="inputItem">
-            <input type="checkbox" id="1" value={1} />
-            <label htmlFor="1">Miód</label>
+            <input type="radio" id="asc" value="asc" name="price" />
+            <label htmlFor="asc">Price: Low to High</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id="desc" value="desc" name="price" />
+            <label htmlFor="desc">Price: High to Low</label>
           </div>
         </div>
+      </div>
       <div className="right">
+        <img
+          className="catImg"
+          src="img/stock-1920-1080/stock-3.jpg"
+          alt=""
+        />
       </div>
-      </div>
-    </div>   )
-}
+    </div>
+)}
